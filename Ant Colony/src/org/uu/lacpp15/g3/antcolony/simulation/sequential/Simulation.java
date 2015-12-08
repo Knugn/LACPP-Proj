8c41ccc20479d89030a5d3a7ce1b084f5ba1f446
David Ryman <David.ryman@hotmail.com>
2015-12-08 18:07:41 +0100

    Moved logic from world to simulation and ai.

--- Ant Colony/src/org/uu/lacpp15/g3/antcolony/simulation/sequential/Simulation.java
+++ Ant Colony/src/org/uu/lacpp15/g3/antcolony/simulation/sequential/Simulation.java
@@ -2,6 +2,8 @@ package org.uu.lacpp15.g3.antcolony.simulation.sequential;
 
 import org.uu.lacpp15.g3.antcolony.simulation.ISimulation;
 import org.uu.lacpp15.g3.antcolony.simulation.IWorld;
+import org.uu.lacpp15.g3.antcolony.simulation.sequential.entities.Ant;
+import org.uu.lacpp15.g3.antcolony.simulation.sequential.entities.EntityIterator;
 
 public class Simulation implements ISimulation {
 	
@@ -24,8 +26,23 @@ public class Simulation implements ISimulation {
 	@Override
 	public void update(long nanoSecDelta) {
 		ai.update(nanoSecDelta);
-		world.update(nanoSecDelta);
+		clampAnts();
+		updatePheromones(nanoSecDelta);
+		//world.update(nanoSecDelta);
 		nanoSecCounter += nanoSecDelta;
+	}
+	
+	private void clampAnts() {
+		EntityIterator<Ant> antsIter = world.getAllAnts().iterator();
+		while (antsIter.next()) {
+			Ant ant = antsIter.getObject();
+			ant.clamp(world.getBounds());
+		}
+	}
+	
+	private void updatePheromones(long nanoSecDelta) {
+		world.getHivePheromoneGrid().update(nanoSecDelta);
+		world.getFoodPheromoneGrid().update(nanoSecDelta);
 	}
 
 	@Override
