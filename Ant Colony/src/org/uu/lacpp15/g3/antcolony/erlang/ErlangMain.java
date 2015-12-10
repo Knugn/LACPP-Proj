3762a9bfe8bbc83648f586f9d1d36c07a966fca7
Anders <Anders@DESKTOP-59G7R5N>
2015-12-10 20:16:15 +0100

    Changed name on erlang main file

---
+++ Ant Colony/src/org/uu/lacpp15/g3/antcolony/erlang/ErlangMain.java
@@ -0,0 +1,26 @@
+package org.uu.lacpp15.g3.antcolony.erlang;
+
+import com.ericsson.otp.erlang.OtpAuthException;
+import com.ericsson.otp.erlang.OtpErlangExit;
+import org.uu.lacpp15.g3.antcolony.client.Client;
+
+import java.io.IOException;
+
+/**
+ * Created by anders on 2015-12-03.
+ */
+public class ErlangMain {
+
+    public static void main(String[] args) throws OtpErlangExit, IOException, OtpAuthException {
+        Client client = new Client(new ErlangSimulation());
+        try {
+            client.run();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+
+    }
+}
