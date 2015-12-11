f8c27c79d3e8914762fce3800ba9f7f720a6f4b5
David Ryman <David.ryman@hotmail.com>
2015-12-11 20:03:35 +0100

    Fixed bug and added more printinfo to see what is actually benchmarked.

--- Ant Colony/src/org/uu/lacpp15/g3/antcolony/benchmark/JavaBenchmark.java
+++ Ant Colony/src/org/uu/lacpp15/g3/antcolony/benchmark/JavaBenchmark.java
@@ -9,8 +9,8 @@ public class JavaBenchmark {
 	
 	public static void main(String[] args) {
 		long nanoSecDelta = 1000000000 / 60;
-		//long maxNanosRunning = (long)1000000000*60*10;
-		long reportInterval = (long)1000000000*10;
+		// long maxNanosRunning = (long)1000000000*60*10;
+		long reportInterval = (long) 1000000000 * 10;
 		int nAnts = 10000, nFood = 10;
 		for (int i = 1; i < args.length; i++) {
 			String[] splitArg = args[i].split("=");
@@ -20,28 +20,36 @@ public class JavaBenchmark {
 					case "nants":
 					case "numants":
 						nAnts = Integer.parseInt(val);
+						break;
 					case "nfood":
 					case "numfood":
 						nFood = Integer.parseInt(val);
+						break;
 					case "reportinterval":
-						reportInterval = (long)Integer.parseInt(val)*1000000000;
+						reportInterval = (long) Integer.parseInt(val) * 1000000000;
+						break;
+					default:
+						throw new IllegalArgumentException("unknown input argument");
 				}
 			}
 		}
-		ISimulation simulation = new Simulation(nAnts,nFood);
+		System.out.println("Running java ant colony simulation benchmark with " + nAnts + " and " + nFood + " food sources.");
+		System.out.println("Report interval: " + reportInterval / 1000000000 + "s");
+		System.out.println();
+		ISimulation simulation = new Simulation(nAnts, nFood);
 		ArrayList<Long> timeSamples = new ArrayList<>(100000);
 		timeSamples.add(System.nanoTime());
-		int lastReportIdx=0;
+		int lastReportIdx = 0;
 		while (true) {
 			simulation.update(nanoSecDelta);
 			long now = System.nanoTime();
 			timeSamples.add(now);
 			if (now - timeSamples.get(lastReportIdx) >= reportInterval) {
-				int nFrames = (timeSamples.size()-lastReportIdx);
+				int nFrames = (timeSamples.size() - lastReportIdx);
 				long avg = (now - timeSamples.get(lastReportIdx)) / nFrames;
 				long min = Long.MAX_VALUE, max = Long.MIN_VALUE;
-				for (int i = lastReportIdx+1; i < timeSamples.size(); i++) {
-					long delta = timeSamples.get(i) - timeSamples.get(i-1);
+				for (int i = lastReportIdx + 1; i < timeSamples.size(); i++) {
+					long delta = timeSamples.get(i) - timeSamples.get(i - 1);
 					if (min > delta)
 						min = delta;
 					if (max < delta)
@@ -49,7 +57,7 @@ public class JavaBenchmark {
 				}
 				String br = System.lineSeparator();
 				StringBuilder sb = new StringBuilder();
-				sb.append("Benchmark report for last " + (reportInterval/1000000000)+"s.");
+				sb.append("Benchmark report for last " + (reportInterval / 1000000000) + "s.");
 				sb.append(br);
 				sb.append("Number of frames: " + nFrames);
 				sb.append(br);
@@ -60,9 +68,9 @@ public class JavaBenchmark {
 				sb.append("Maximum frame time: " + max + "ns");
 				sb.append(br);
 				System.out.println(sb.toString());
-				lastReportIdx = timeSamples.size()-1;
+				lastReportIdx = timeSamples.size() - 1;
 			}
-		} //while (timeSamples.get(timeSamples.size()-1)-timeSamples.get(0) < maxNanosRunning);
+		} // while (timeSamples.get(timeSamples.size()-1)-timeSamples.get(0) < maxNanosRunning);
 	}
 	
 }
