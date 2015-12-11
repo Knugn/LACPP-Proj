3564722ed5d452f801154a87a69222a2f7c83ba6
Anders <Anders@DESKTOP-59G7R5N>
2015-12-11 21:57:05 +0100

    removed erlang code agiens

--- Ant Colony/src/org/uu/lacpp15/g3/antcolony/erlang/ErlangFood.java
+++ Ant Colony/src/org/uu/lacpp15/g3/antcolony/erlang/ErlangFood.java
@@ -1,70 +1,70 @@
-package org.uu.lacpp15.g3.antcolony.erlang;
-
-import org.uu.lacpp15.g3.antcolony.simulation.entities.IREntityIterator;
-import org.uu.lacpp15.g3.antcolony.simulation.entities.IRFoodSources;
-
-/**
- * Created by anders on 2015-12-04.
- */
-public class ErlangFood implements IRFoodSources {
-
-    private ErlangEntry[] entrys;
-
-
-    public ErlangFood(ErlangEntry[] entrys){
-        this.entrys = entrys;
-    }
-
-
-    @Override
-    public int size() {
-        return entrys.length;
-    }
-
-    @Override
-    public IREntityIterator iterator() {
-        return new ErlangEntryIterator();
-    }
-
-
-
-    public class ErlangEntryIterator implements IREntityIterator{
-
-        int index = -1;
-
-        @Override
-        public boolean next() {
-            return ++index < entrys.length;
-        }
-
-        @Override
-        public long getId() {
-            return index;
-        }
-
-        @Override
-        public int getx() {
-            return getCoord(0);
-        }
-
-        @Override
-        public int gety() {
-            return getCoord(1);
-        }
-
-        @Override
-        public int getCoord(int n) {
-            if(n==0){
-                return entrys[index].getX();
-            }else {
-                return  entrys[index].getY();
-            }
-        }
-
-        @Override
-        public float getRadius() {
-            return 4;
-        }
-    }
-
-}
+//package org.uu.lacpp15.g3.antcolony.erlang;
+//
+//import org.uu.lacpp15.g3.antcolony.simulation.entities.IREntityIterator;
+//import org.uu.lacpp15.g3.antcolony.simulation.entities.IRFoodSources;
+//
+///**
+// * Created by anders on 2015-12-04.
+// */
+//public class ErlangFood implements IRFoodSources {
+//
+//    private ErlangEntry[] entrys;
+//
+//
+//    public ErlangFood(ErlangEntry[] entrys){
+//        this.entrys = entrys;
+//    }
+//
+//
+//    @Override
+//    public int size() {
+//        return entrys.length;
+//    }
+//
+//    @Override
+//    public IREntityIterator iterator() {
+//        return new ErlangEntryIterator();
+//    }
+//
+//
+//
+//    public class ErlangEntryIterator implements IREntityIterator{
+//
+//        int index = -1;
+//
+//        @Override
+//        public boolean next() {
+//            return ++index < entrys.length;
+//        }
+//
+//        @Override
+//        public long getId() {
+//            return index;
+//        }
+//
+//        @Override
+//        public int getx() {
+//            return getCoord(0);
+//        }
+//
+//        @Override
+//        public int gety() {
+//            return getCoord(1);
+//        }
+//
+//        @Override
+//        public int getCoord(int n) {
+//            if(n==0){
+//                return entrys[index].getX();
+//            }else {
+//                return  entrys[index].getY();
+//            }
+//        }
+//
+//        @Override
+//        public float getRadius() {
+//            return 4;
+//        }
+//    }
+//
+//}
--- Ant Colony/src/org/uu/lacpp15/g3/antcolony/erlang/SimpleErlangMap.java
+++ Ant Colony/src/org/uu/lacpp15/g3/antcolony/erlang/SimpleErlangMap.java
@@ -1,94 +1,94 @@
-package org.uu.lacpp15.g3.antcolony.erlang;
-
-import org.uu.lacpp15.g3.antcolony.common.IRAABoxInt2;
-import org.uu.lacpp15.g3.antcolony.simulation.IRPheromoneGrid;
-import org.uu.lacpp15.g3.antcolony.simulation.IWorld;
-import org.uu.lacpp15.g3.antcolony.simulation.entities.IRAnts;
-import org.uu.lacpp15.g3.antcolony.simulation.entities.IREntities;
-import org.uu.lacpp15.g3.antcolony.simulation.entities.IRFoodSources;
-import org.uu.lacpp15.g3.antcolony.simulation.entities.IRHives;
-import org.uu.lacpp15.g3.antcolony.simulation.sequential.WorldBounds;
-
-/**
- * Created by anders on 2015-12-03.
- */
-public class SimpleErlangMap implements IWorld {
-
-    private ErlangAnts ants;
-    private ErlangFood food;
-    private WorldBounds box;
-    private ErlangHives hives;
-    private ErlangPheromone hivePheromoneGrid;
-    private ErlangPheromone foodPheromone;
-
-
-    public SimpleErlangMap(WorldBounds box, int n,int hiveX,int hiveY,ErlangFood food){
-
-        this.box = box;
-
-        ants = new ErlangAnts(n);
-
-        ErlangHive hive = new ErlangHive(hiveX,hiveY);
-        ErlangHive[] hiveArray = new ErlangHive[1];
-        hiveArray[0] = hive;
-        hives = new ErlangHives(hiveArray);
-
-
-        this.food = food;
-        hivePheromoneGrid = new ErlangPheromone(box);
-        foodPheromone = new ErlangPheromone(box);
-    }
-
-    public void setAnt(double x, double y){
-        ants.set(x,y);
-    }
-
-    public void reset(){
-        ants.reset();
-        hivePheromoneGrid.restet();
-        foodPheromone.restet();
-    }
-
-    public void addFoodPheromon(int x,int y,float strength){
-        foodPheromone.add(x,y,strength);
-    }
-
-    public void addHivePheromon(int x,int y,float strength){
-        hivePheromoneGrid.add(x,y,strength);
-    }
-
-    @Override
-    public IRAABoxInt2 getBounds() {
-        return box;
-    }
-
-    @Override
-    public IREntities getAllEntities() {
-        return null;
-    }
-
-    @Override
-    public IRAnts getAllAnts() {
-        return ants;
-    }
-
-    @Override
-    public IRHives getAllHives() {
-        return hives;
-    }
-
-    @Override
-    public IRFoodSources getAllFoodSources() {
-        return food;
-    }
-
-    @Override
-    public IRPheromoneGrid getHivePheromoneGrid() {
-        return hivePheromoneGrid;
-    }
-
-    @Override
-    public IRPheromoneGrid getFoodPheromoneGrid() {
-        return foodPheromone;
-    }
-}
+//package org.uu.lacpp15.g3.antcolony.erlang;
+//
+//import org.uu.lacpp15.g3.antcolony.common.IRAABoxInt2;
+//import org.uu.lacpp15.g3.antcolony.simulation.IRPheromoneGrid;
+//import org.uu.lacpp15.g3.antcolony.simulation.IWorld;
+//import org.uu.lacpp15.g3.antcolony.simulation.entities.IRAnts;
+//import org.uu.lacpp15.g3.antcolony.simulation.entities.IREntities;
+//import org.uu.lacpp15.g3.antcolony.simulation.entities.IRFoodSources;
+//import org.uu.lacpp15.g3.antcolony.simulation.entities.IRHives;
+//import org.uu.lacpp15.g3.antcolony.simulation.sequential.WorldBounds;
+//
+///**
+// * Created by anders on 2015-12-03.
+// */
+//public class SimpleErlangMap implements IWorld {
+//
+//    private ErlangAnts ants;
+//    private ErlangFood food;
+//    private WorldBounds box;
+//    private ErlangHives hives;
+//    private ErlangPheromone hivePheromoneGrid;
+//    private ErlangPheromone foodPheromone;
+//
+//
+//    public SimpleErlangMap(WorldBounds box, int n,int hiveX,int hiveY,ErlangFood food){
+//
+//        this.box = box;
+//
+//        ants = new ErlangAnts(n);
+//
+//        ErlangHive hive = new ErlangHive(hiveX,hiveY);
+//        ErlangHive[] hiveArray = new ErlangHive[1];
+//        hiveArray[0] = hive;
+//        hives = new ErlangHives(hiveArray);
+//
+//
+//        this.food = food;
+//        hivePheromoneGrid = new ErlangPheromone(box);
+//        foodPheromone = new ErlangPheromone(box);
+//    }
+//
+//    public void setAnt(double x, double y){
+//        ants.set(x,y);
+//    }
+//
+//    public void reset(){
+//        ants.reset();
+//        hivePheromoneGrid.restet();
+//        foodPheromone.restet();
+//    }
+//
+//    public void addFoodPheromon(int x,int y,float strength){
+//        foodPheromone.add(x,y,strength);
+//    }
+//
+//    public void addHivePheromon(int x,int y,float strength){
+//        hivePheromoneGrid.add(x,y,strength);
+//    }
+//
+//    @Override
+//    public IRAABoxInt2 getBounds() {
+//        return box;
+//    }
+//
+//    @Override
+//    public IREntities getAllEntities() {
+//        return null;
+//    }
+//
+//    @Override
+//    public IRAnts getAllAnts() {
+//        return ants;
+//    }
+//
+//    @Override
+//    public IRHives getAllHives() {
+//        return hives;
+//    }
+//
+//    @Override
+//    public IRFoodSources getAllFoodSources() {
+//        return food;
+//    }
+//
+//    @Override
+//    public IRPheromoneGrid getHivePheromoneGrid() {
+//        return hivePheromoneGrid;
+//    }
+//
+//    @Override
+//    public IRPheromoneGrid getFoodPheromoneGrid() {
+//        return foodPheromone;
+//    }
+//}
